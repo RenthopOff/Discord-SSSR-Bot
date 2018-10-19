a146b770d20f9221ff3d473df78b8878451a70ef
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-10-19 19:55:04 +0300

    Update player.js

--- modules/player.js
+++ modules/player.js
@@ -2,13 +2,17 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
+  let user = message.mentions.users.first() || message.author;
+
   let playerembed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setDescription("Информация об гражданском")
   .setColor("#4b0082")
   .setThumbnail(message.author.avatarURL)
-  .addField("Полное Имя", message.author.tag)
+  .addField("Полное Имя", message.author.tag, true)
   .addField("ID", message.author.id)
+  .addField("Статус Гражданина:", user.presence.status, true)
+  .addField("Сейчас играет:", `${user.presence.game ? user.presence.game.name: 'Сидит Пердит!'}`)
   .addField("Зашел", message.author.createdAt);
 
   message.channel.send(playerembed);
