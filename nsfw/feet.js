744a2c2402176cf5ab7c6177455d3f7f012fc3cd
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-10-19 14:44:29 +0300

    Add files via upload

---
+++ nsfw/feet.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+
+module.exports.run = async (bot,message,args) => {
+  if (!message.channel.nsfw) return message.channel.send('Используйте в специальных каналах к примеру #🔞pron!')
+  let {body} = await superagent
+  .get (`https://nekos.life/api/v2/img/feet`);
+  let dogembed = new Discord.RichEmbed()
+  .setColor("#911e42")
+  .setTitle("Ohh... :sweat_drops: ")
+  .setImage(body.url);
+
+  message.channel.send(dogembed);
+
+}
+
+module.exports.help = {
+  name: "feet",
+}
