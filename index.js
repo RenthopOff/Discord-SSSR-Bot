adf5cec6fd2d9d2dd5d4a8238b22162f6e642331
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-11-02 18:30:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const botconfig = require('./botconfig.json');
 const Languages = require('./Languages.json');
 const ms = require("ms");
 const mongoose = require('mongoose');
-const steam = require('steam-provider');
 const prefix = botconfig.prefix;
 const Attachment = require('discord.js');
 const RichEmbed = require('discord.js');
