76843335171164c8bfc6324a82a531a9f083cb7b
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-11-02 09:04:43 +0300

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args, ops) => {
+
+	if (!message.member.roles.find("name", "@everyone")) { //Whatever role you want, I pick @everyone because everyone can use this command
+		message.channel.send('Недостаточно прав у вас.');
+		return;
+	}
+
+    // Check for input
+    if (!args[0]) return message.channel.send('Proper usage: a.poll <question>');
+
+    // Create Embed
+    const embed = new Discord.RichEmbed()
+        .setColor("#ffffff") //To change color do .setcolor("#fffff")
+        .setFooter('React to Vote.')
+        .setDescription(args.join(' '))
+        .setTitle(`Голосование создал: ${message.author.username}`);
+
+    let msg = await message.channel.send(embed)
+        .then(function (msg) {
+            msg.react("❎");
+            msg.react("✅"); // You can only add two reacts
+            message.delete({timeout: 1000});
+            }).catch(function(error) {
+            console.log(error);
+        });
+};
+
+exports.help = {
+  name:"poll"
+}
