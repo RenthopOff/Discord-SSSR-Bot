ef753d0d23c5da934691ee0bb41ffb0c645a07b2
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-11-17 13:46:07 +0300

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,24 +1,25 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args, ops) => {
-	if(!message.member.hasPermission("ADMINISTRATOR")){
-		if (!message.member.roles.find("name", "@everyone")) {
-			message.channel.send('Недостаточно прав у вас.');
-			return;
-		}
-		
-    if (!args[0]) return message.channel.send('Proper usage: a.poll <вопрос>');
 
+	if (!message.member.roles.find("name", "👑 Маршал Российской Федерации 👑")) {
+		message.channel.send('Invalid permissions.');
+		return;
+	}
+
+
+    if (!args[0]) return message.channel.send('Proper usage: a.poll <question>');
+		if(!message.member.hasPermission("ADMINISTRATOR")){
     const embed = new Discord.RichEmbed()
-        .setColor("#ffffff") //To change color do .setcolor("#fffff")
-        .setFooter('Реакции для голосование')
+        .setColor("#560319") 
+        .setFooter('Реакции для голосавания')
         .setDescription(args.join(' '))
-        .setTitle(`Голосование создал: ${message.author.username}`);
+        .setTitle(`Poll Created By ${message.author.username}`);
 
     let msg = await message.channel.send(embed)
         .then(function (msg) {
             msg.react("❎");
-            msg.react("✅"); // You can only add two reacts
+            msg.react("✅"); 
             message.delete({timeout: 1000});
             }).catch(function(error) {
             console.log(error);
