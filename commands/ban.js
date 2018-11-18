74e70e7f9f2d621d597c0d1412b1deef81507233
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-11-18 17:12:15 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    
+    let replies = ["http://images.vfl.ru/ii/1541862727/3540fecf/24139903.jpg", "http://images.vfl.ru/ii/1541862804/962938a3/24139923.jpg", "http://images.vfl.ru/ii/1541862873/a0b58aa7/24139953.jpg", "http://images.vfl.ru/ii/1541862903/e9ada845/24139968.jpg"];
+
+    let result = Math.floor((Math.random() * replies.length));
 
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Надо иметь права : **ADMINISTRATOR**");
     if(args[0] == "help"){
@@ -8,11 +12,7 @@ module.exports.run = async (bot, message, args) => {
       return;
     }
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    let day = message.guild.createdAt.getDate();
-    
-    let result = Math.floor((Math.random() * replies.length));
-    let replies = ["http://images.vfl.ru/ii/1541862727/3540fecf/24139903.jpg", "http://images.vfl.ru/ii/1541862804/962938a3/24139923.jpg", "http://images.vfl.ru/ii/1541862873/a0b58aa7/24139953.jpg", "http://images.vfl.ru/ii/1541862903/e9ada845/24139968.jpg"];
-    
+    let day = message.guild.createdAt.getDate(): 
     let month = 1 + message.guild.createdAt.getMonth();
     let year = message.guild.createdAt.getFullYear();
     if(!bUser) return message.channel.send("Не могу найти его!");
