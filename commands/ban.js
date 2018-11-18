79d4f48d8beb8119d5c7a4e92cfbc683fd8fdaf3
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-11-18 15:54:08 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,15 +2,18 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Надо иметь права : **ADMINISTRATOR**");
     if(args[0] == "help"){
       message.reply("Usage: !ban <user> <reason>");
       return;
     }
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+     let day = message.guild.createdAt.getDate();
+    let month = 1 + message.guild.createdAt.getMonth();
+    let year = message.guild.createdAt.getFullYear();
     if(!bUser) return message.channel.send("Не могу найти его!");
     let bReason = args.join(" ").slice(22);
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("У него привилегия выше твоей,LOL!?");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("У него привилегия выше твоей!");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
@@ -18,8 +21,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Забанен", `${bUser} with ID ${bUser.id}`)
     .addField("Кем был выдан бан", `<@${message.author.id}> with ID ${message.author.id}`)
     .addField("Забанен в", message.channel)
-    .addField("Время", message.createdAt)
-    .addField("Прчина", bReason);
+    .setFooter(`Время бана • ${day}.${month}.${year}`)
+    .addField("Причина", bReason);
 
     let incidentchannel = message.guild.channels.find(`name`, "incidents");
     if(!incidentchannel) return message.channel.send("Не могу найти канал где это произошло.");
