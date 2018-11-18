1ec072c514be36adeaa265a0c1119e2ad449c833
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2018-11-18 17:09:33 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -9,8 +9,10 @@ module.exports.run = async (bot, message, args) => {
     }
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let day = message.guild.createdAt.getDate();
+    
     let result = Math.floor((Math.random() * replies.length));
     let replies = ["http://images.vfl.ru/ii/1541862727/3540fecf/24139903.jpg", "http://images.vfl.ru/ii/1541862804/962938a3/24139923.jpg", "http://images.vfl.ru/ii/1541862873/a0b58aa7/24139953.jpg", "http://images.vfl.ru/ii/1541862903/e9ada845/24139968.jpg"];
+    
     let month = 1 + message.guild.createdAt.getMonth();
     let year = message.guild.createdAt.getFullYear();
     if(!bUser) return message.channel.send("Не могу найти его!");
@@ -24,8 +26,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Забанен", `${bUser} with ID ${bUser.id}`)
     .addField("Кем был выдан бан", `<@${message.author.id}> with ID ${message.author.id}`)
     .addField("Забанен в", message.channel)
-    .setImage(replies[result])
-    .addField("Причина", bReason);
+    .addField("Причина", bReason)
+    .setImage(replies[result]);
 
     let incidentchannel = message.guild.channels.find(`name`, "📥ссылки");
     if(!incidentchannel) return message.channel.send("Не могу найти канал где это произошло.");
