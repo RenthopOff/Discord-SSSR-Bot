69619ee63ab0e5f387246eb58666dbd6fcf95ffe
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-01-04 11:59:17 +0300

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -5,8 +5,9 @@ module.exports.run = async (bot, message, args) => {
   if(message.member.hasPermission("ADMINISTRATOR")) {
       let rulesembed = new Discord.RichEmbed()
       .setColor("#8000ff")
-      .addField("**Приветствуем тебя в нашем ламповом месте, где ты можешь отдохнуть и поиграть в игры или побеседовать с другими людьми.", true)
-      .addField("Первое дело пожалуйста прочти правила и потом можешь идти со спокойной душой развлекаться.")
+      .setTitle("**Приветствуем тебя в нашем ламповом месте, где ты можешь отдохнуть и поиграть в игры или побеседовать с другими людьми.")
+      .setTitle("Первое дело пожалуйста прочти правила и потом можешь идти со спокойной душой развлекаться.");
+      
       message.channel.send(rulesembed)
     }
 
@@ -15,3 +16,4 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name:"wel"
 }
+
