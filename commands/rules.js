0ff9051703bb31badb670791a4e33a6947599c2a
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-01-04 09:48:27 +0300

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if(message.member.hasPermission("ADMINISTRATOR")) {
       let rulesembed = new Discord.RichEmbed()
       .setColor("FF0000")
-      .addField("1.1 Запрещён спам, флуд, злоупотребление капсом в чате кроме", true)
+      .addFiled("**Общение**", `1.1 Запрещён спам, флуд, злоупотребление капсом в чате кроме \n1.2 Запрещено ставить провокационные и оскорбительные никнеймы.\n1.3 Запрещено несоблюдение тематики чата.\n 1.4 Трансляция музыки/видео через микрофон.\n 1.5 Неадекватное поведение в текстовом или голосовом чате в любых проявлениях.`
       .setDescription(args.join(" "));
 
       message.channel.send(rulesembed)
