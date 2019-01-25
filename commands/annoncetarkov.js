04fca19feaac3e5c4df5f06481b76f3e3417c36b
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-01-25 15:10:11 +0300

    Update annoncetarkov.js

--- commands/annoncetarkov.js
+++ commands/annoncetarkov.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(message.member.hasPermission("ADMINISTRATOR")) {
+      let rulesembed = new Discord.RichEmbed()
+      .setColor("#8000ff")
+      .addField("[**Нововведение в игре**]", `<@&506168508892184586> - новое в игре`)
+      .setImage("https://www.free-wallpapers.su/data/media/5477/big/gm6021.jpg")
+      message.channel.send(rulesembed)
+    }
+
+}
+
+module.exports.help = {
+  name:"annoncegametarkov"
+}
 
