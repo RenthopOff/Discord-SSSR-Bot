e016fb4f521b7c0c954c9b2fdce37d1c7ce8dc3e
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-01-27 08:58:12 +0300

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
   if(message.member.hasPermission("ADMINISTRATOR")) {
       let rulesembed = new Discord.RichEmbed()
       .setColor("#8000ff")
-      .addField("[**Нововведение в игре**]", `<@&538320686150713356> - новое в игре`)
-      .setImage("https://i.pinimg.com/originals/f3/62/06/f36206d27a5d40908bd1bc02ecca2b27.jpg")
+      .addField("[**Нововведение в игре**]", `<@&538320686150713356> - Новый рейтинговый сезон начнётся на следующей неделе! Также в игре выйдет патч 7.21. Долгожданного героя Mars обещают добавить в игру в конце февраля или начале марта.`)
+      .setImage("https://sun1-14.userapi.com/c848620/v848620236/10d629/BEQ2kguWecM.jpg")
       message.channel.send(rulesembed)
     }
 
