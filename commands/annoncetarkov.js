ee0c8532ea7ad08811186999cb174da4f31c389e
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-01-29 16:14:55 +0300

    Update annoncetarkov.js

--- commands/annoncetarkov.js
+++ commands/annoncetarkov.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if(message.member.hasPermission("ADMINISTRATOR")) {
       let rulesembed = new Discord.RichEmbed()
       .setColor("#8000ff")
-      .addField("[**Нововведение в игре**]", `<@&506168508892184586> - новое в игре`)
+      .addField("[**Нововведение в игре**]", `<@&506168508892184586> - я знаю что новость не новая,но кто вдруг не видел данный обзор на ближайшие обновление EFT то вот вам видеролик\n https://www.youtube.com/watch?time_continue=14&v=KwqxxcVb5fM`)
       .setImage("https://www.free-wallpapers.su/data/media/5477/big/gm6021.jpg")
       message.channel.send(rulesembed)
     }
