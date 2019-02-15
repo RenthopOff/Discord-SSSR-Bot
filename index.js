46f5dae93d9000263f1496ee84d0d95ae5ee9760
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-02-15 17:30:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,24 @@ fs.readdir("./commands/", (err, files) => {
     });
 
 });
+fs.readdir("./welcomer/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./welcomer/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+
+    });
+
+});
 fs.readdir("./nsfw/", (err, files) => {
 
   if(err) console.log(err);
