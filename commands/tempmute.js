25532433996726ac82fb7d7ef4d59e9c65bd2f40
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-02-16 11:22:22 +0300

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Не могу его трогать!");
   let muterole = message.guild.roles.find(`name`, "muted");
   //start of create role
   if(!muterole){
@@ -29,14 +29,14 @@ module.exports.run = async (bot, message, args) => {
   }
   //end of create role
   let mutetime = args[1];
-  if(!mutetime) return message.reply("You didn't specify a time!");
+  if(!mutetime) return message.reply("Поставь время!");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  message.reply(`<@${tomute.id}> был выдан мут на: ${ms(ms(mutetime))}`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.channel.send(`<@${tomute.id}> размучен!`);
   }, ms(mutetime));
 
 
