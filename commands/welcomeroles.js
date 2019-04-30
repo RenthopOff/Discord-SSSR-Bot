ff3dd6b168449c0b9e78956825a2a47beca3d6ec
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-04-30 17:29:34 +0300

    Update welcomeroles.js

--- commands/welcomeroles.js
+++ commands/welcomeroles.js
@@ -1,41 +1,87 @@
 const { RichEmbed } = require('discord.js');
+const { prefix } = require('../botconfig');
 
 exports.run = async (bot, message, args) => {
 
-    /*
-        1) Use the messageReactionAdd and messageReactionRemove events to add/remove users roles
-        2) Remove the awaitReactions() function as we won't need that anymore
-        3) Customize the message a bit more to fit a general welcome channel
-    */
-
-    await message.delete().catch(O_o => {});
+    await message.delete().catch(O_o=>{});
 
     const a = message.guild.roles.get('495605551774826519'); // Moderator
-    const b = message.guild.roles.get('485987998165499914'); // Administrator
+    const b = message.guild.roles.get('495199722458316825'); // Administrator
     const c = message.guild.roles.get('538320686150713356'); // Developer
 
+    const filter = (reaction, user) => ['🇦', '🇧', '🇨'].includes(reaction.emoji.name) && user.id === message.author.id;
+
     const embed = new RichEmbed()
-        .setTitle('Available Roles')
+        .setTitle('Avaiilable Roles')
         .setDescription(`
-       
-        Welcome to **${message.guild.name}**! You may choose from our list of roles you can join/leave from:
-
-       🇦 ${a.toString()}
-       🇧 ${b.toString()}
-       🇨 ${c.toString()}
-
-       `)
+        
+        🇦 ${a.toString()}
+        🇧 ${b.toString()}
+        🇨 ${c.toString()}
+        `)
         .setColor(0xdd9323)
-        .setFooter(`Guild ID: ${message.guild.id}`);
-
+        .setFooter(`ID: ${message.author.id}`);
+        
     message.channel.send(embed).then(async msg => {
 
         await msg.react('🇦');
         await msg.react('🇧');
         await msg.react('🇨');
+
+        msg.awaitReactions(filter, {
+            max: 1,
+            time: 30000,
+            errors: ['time']
+        }).then(collected => {
+
+            const reaction = collected.first();
+
+            switch (reaction.emoji.name) {
+                case '🇦':
+                    if (message.member.roles.has(a.id)) {
+                        msg.delete(2000);
+                        return message.channel.send('You are already in this role!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(a).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Error adding you to this role: **${err.message}**.`);
+                    });
+                    message.channel.send(`You have been added to the **${a.name}** role!`).then(m => m.delete(3000));
+                    msg.delete();
+                    break;
+                case '🇧':
+                    if (message.member.roles.has(b.id)) {
+                        msg.delete(2000);
+                        return message.channel.send('You are already in this role!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(b).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Error adding you to this role: **${err.message}**.`);
+                    });
+                    message.channel.send(`You have been added to the **${b.name}** role!`).then(m => m.delete(3000));
+                    msg.delete();
+                    break;
+                case '🇨':
+                    if (message.member.roles.has(c.id)) {
+                        msg.delete(2000);
+                        return message.channel.send('You are already in this role!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(c).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Error adding you to this role: **${err.message}**.`);
+                    });
+                    message.channel.send(`You have been added to the **${c.name}** role!`).then(m => m.delete(3000));
+                    msg.delete();
+                    break;
+            }
+        }).catch(collected => {
+            return message.channel.send(`I couldn't add you to this role!`);
+        });
+
     });
+
 };
 
 exports.help = {
-    name: 'welcomeroles'
+    name: 'roles'
 };
