06e4378e47d8e373a33912f345cc41281d2626e3
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-05-03 11:01:26 +0300

    Очистка чата

--- modules/clear.js
+++ modules/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  if(!args[0]) return message.channel.send("oof");
+  if(!args[0]) return message.channel.send("Упс...");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Убрано ${args[0]} сообщений.`).then(msg => msg.delete(3000));
   });
