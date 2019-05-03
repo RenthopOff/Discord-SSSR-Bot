f1a98c61f9c4fe619f42133a24da1c1f729afc97
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-05-03 14:12:59 +0300

    Update clear.js

--- modules/clear.js
+++ modules/clear.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Недостаточно прав на использование команды!')
   if(!args[0]) return message.channel.send("Упс...");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Убрано ${args[0]} сообщений.`).then(msg => msg.delete(3000));
