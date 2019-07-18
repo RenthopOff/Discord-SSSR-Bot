1632ae298437f1cf613eff50a763a947788d8430
RenthopOff <44197289+RenthopOff@users.noreply.github.com>
2019-07-18 13:21:08 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 let profile = require("../profile.json");
 module.exports.run = async (bot,message,args) => {
     try{
-      
+        
+    var canal = message.guild.channels.find('name', '📥доносы')
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("У вас нет прав");
     let rUser = bot.rUser;
     if(!args[0]) return bot.send("Вы не указали пользователя");
     if(!rUser) return bot.send("Пользователь не найден");
-    if(!profile[rUser.id])return bot.send("Пользователя нету в profile.json");
+    if(!profile[rUser.id])return bot.send("Пользователя нету в базе данных");
     profile[rUser.id].warns++;
     fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
         if(err) console.log(err);
@@ -23,7 +24,7 @@ module.exports.run = async (bot,message,args) => {
     .addField("Выдал предупреждение",`${rUser.user.username}`)
     .addField("Количество предупрежденией",`${profile[rUser.id].warns}/3`);
 
-    message.channel.send(embed);
+    canal.send(embed);
     }catch(err){
         console.log(`1.${err.name}\n2.${err.message}\n3.${err.stack}`);
     }
